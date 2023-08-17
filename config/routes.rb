12413a4bc01fe1ffb73561e58d8d39180e7723c2
urunyon <141293154+urunyon@users.noreply.github.com>
2023-08-17 02:07:43 +0000

    3-2-14

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: "homes#top"#TOPページを/にしてる
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   get 'homes/about', as: 'about'#　homesコントローラ/aboutアクション,as: aboutパス
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
 end
